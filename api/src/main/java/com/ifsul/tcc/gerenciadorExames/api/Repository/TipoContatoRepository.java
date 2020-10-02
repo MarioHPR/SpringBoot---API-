fd5a4b2c5e84e99f9a283d9158d28cb72f3310e8
mariohpr <mariopereira398@gmail.com>
2020-10-01 23:46:37 -0300

    implementação testes unitario de TipoContato.

--- api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/TipoContatoRepository.java
+++ api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/TipoContatoRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TipoContatoRepository extends CrudRepository<TipoContato,Integer> {
+    TipoContato findByNome(String nome);
 }
