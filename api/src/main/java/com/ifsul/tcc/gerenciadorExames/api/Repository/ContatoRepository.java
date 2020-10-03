a13559f9dc2cebd33e4a6e8630bba9092d299365
mariohpr <mariopereira398@gmail.com>
2020-10-02 23:40:13 -0300

    implementação metodos para utilizar em testes com ContatoRepository

--- api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/ContatoRepository.java
+++ api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/ContatoRepository.java
@@ -1,9 +1,17 @@
 package com.ifsul.tcc.gerenciadorExames.api.Repository;
 
 import com.ifsul.tcc.gerenciadorExames.api.Entity.Contato;
+import com.ifsul.tcc.gerenciadorExames.api.Entity.TipoContato;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ContatoRepository extends CrudRepository<Contato,Integer> {
+    Optional<Contato> findById(Integer id);
+    Contato findByValor(String valor);
+    List<Contato> findAllByTipoContato(TipoContato tipoContato);
+    List<Contato> findAll();
 }
