b6a8bd8b2415bf013da89261e9a6efc4a3ad2b35
mariohpr <mariopereira398@gmail.com>
2020-10-04 17:58:30 -0300

    Implementacao para testes ConsultaRepository.

--- api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/ConsultaRepository.java
+++ api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/ConsultaRepository.java
@@ -4,6 +4,10 @@ import com.ifsul.tcc.gerenciadorExames.api.Entity.Consulta;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConsultaRepository extends CrudRepository<Consulta, Integer> {
+    Consulta findByDiagnostico(String diagnostico);
+    List<Consulta> findAllByNomeMedico(String nomeMedico);
 }
