d177e91f47bc5bc381d35d209971a8db33dae2cc
mariohpr <mariopereira398@gmail.com>
2020-10-04 18:39:07 -0300

    Implementação metodos TipoExameRepository.

--- api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/TipoExameRepository.java
+++ api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/TipoExameRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TipoExameRepository extends CrudRepository<TipoExame, Integer> {
+    TipoExame findByNomeExame(String nomeExame);
 }
