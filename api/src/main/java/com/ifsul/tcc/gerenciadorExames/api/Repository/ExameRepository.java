4e46eda6efa05e46a6eec3eedea4c5e601eb8371
mariohpr <mariopereira398@gmail.com>
2020-10-05 23:32:51 -0300

    implementação de metodos em ExameRepository.

--- api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/ExameRepository.java
+++ api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/ExameRepository.java
@@ -1,9 +1,15 @@
 package com.ifsul.tcc.gerenciadorExames.api.Repository;
 
 import com.ifsul.tcc.gerenciadorExames.api.Entity.Exame;
+import com.ifsul.tcc.gerenciadorExames.api.Entity.TipoExame;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.List;
+
 @Repository
 public interface ExameRepository extends CrudRepository<Exame, Integer> {
+    Exame findByDataExame(Date dataExame);
+    List<Exame> findAllByTipoExame(TipoExame tipoExame);
 }
