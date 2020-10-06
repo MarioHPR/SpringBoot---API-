c79411ad1907c6952323f1ef255c02f17a0217e0
mariohpr <mariopereira398@gmail.com>
2020-10-06 00:28:16 -0300

    implementação de metodos em ParametroExameRepository.

--- api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/ParametroExameRepository.java
+++ api/src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/ParametroExameRepository.java
@@ -1,9 +1,13 @@
 package com.ifsul.tcc.gerenciadorExames.api.Repository;
 
+import com.ifsul.tcc.gerenciadorExames.api.Entity.CampoParametro;
+import com.ifsul.tcc.gerenciadorExames.api.Entity.Exame;
 import com.ifsul.tcc.gerenciadorExames.api.Entity.ParametroExame;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ParametroExameRepository extends CrudRepository<ParametroExame, Integer> {
+    ParametroExame findByExame(Exame exame);
+    ParametroExame findByCampoParametro(CampoParametro campoParametro);
 }
